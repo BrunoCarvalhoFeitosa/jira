0e6003a5c6550fa13d9c25d820335d37f5c55e73
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2025-10-20 18:46:57 -0300

    feat change color of github icon

--- components/auth/sign-in-card/index.tsx
+++ components/auth/sign-in-card/index.tsx
@@ -142,7 +142,7 @@ export const SignInCard = () => {
                 className="has-[>svg]:px-0 flex justify-center items-center w-12 h-12 rounded-full bg-white border-gray-50 cursor-pointer"
                 onClick={() => signUpWithGitHub()}
               >
-                <FaGithub className="size-7 text-blue-600" />
+                <FaGithub className="size-7 text-black" />
               </Button>
             </div>
           </div>
--- components/auth/sign-up-card/index.tsx
+++ components/auth/sign-up-card/index.tsx
@@ -210,7 +210,7 @@ export const SignUpCard = () => {
                 className="has-[>svg]:px-0 flex justify-center items-center w-12 h-12 rounded-full bg-white border-gray-50 cursor-pointer"
                 onClick={() => signUpWithGitHub()}
               >
-                <FaGithub className="size-7 text-blue-600" />
+                <FaGithub className="size-7 text-black" />
               </Button>
             </div>
           </div>
